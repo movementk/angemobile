003d702f9c26eae2764005e66ba0fa88a2dc0de3
Joahra <mk_ahra0522@gmail.com>
2018-01-28 18:49:14 +0900

    모바일 앙쥬 백서 두페이지 완료.

---
+++ whitepaper/first_ange.php
@@ -0,0 +1,111 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead.php"); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/whitepaper.css" rel="stylesheet">
+</head>
+<body class="sub whitepaper"><!-- 폴더별 클래스명 추가 -->
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/header.php"); ?>
+    <!-- pgae-header -->
+    <div class="page-header">
+        <!-- 폴더별 타이틀 -->
+        <h2>앙쥬 처음이세요?</h2>
+        <div class="btn-controls">
+            <a href="#" class="prev">
+                <i class="icon-left-open-big">
+                    <span class="sr-only">이전 페이지</span>
+                </i>
+            </a>
+            <a href="#" class="next">
+                <i class="icon-right-open-big">
+                    <span class="sr-only">다음 페이지</span>
+                </i>
+            </a>
+        </div>
+    </div>
+    
+    <!-- sub content start -->
+    <main id="content">
+        <div class="container">
+            <section class="first-ange"><!-- 페이지명 클래스 -->
+                <div class="section-header">
+                    <p>
+                        안녕하세요 앙쥬 입니다. <br>
+                        앙쥬는 엄마들에게 꼭 필요한 임신/출산/육아/ 리빙 정보를 공유하며
+                        임산부 육아맘 모두가 <i>정보 공유 및 소통할 수 있는 공간</i>입니다.
+                    </p>
+                </div>
+                <div class="section-content">
+                    <ul>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/whitepaper/e_magazine.png" alt="">
+                            </figure>
+                            <dl>
+                                <dt>
+                                    <span>01</span>e-매거진
+                                </dt>
+                                <dd>매월 출간되는 앙쥬 잡지 컨텐츠를 한눈에 볼 수 있습니다.</dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/whitepaper/community.png" alt="">
+                            </figure>
+                            <dl>
+                                <dt>
+                                    <span>02</span>커뮤니티
+                                </dt>
+                                <dd>임신, 출산, 육아에 궁금하신 점이나 공유하고 싶은 이야기들을 나누는 공간입니다.</dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/whitepaper/event.png" alt="">
+                            </figure>
+                            <dl>
+                                <dt>
+                                    <span>03</span>체험단/이벤트
+                                </dt>
+                                <dd>임신,출산,육아에 필요한 다양한 제품들을 체험할 수 있는 기회와 각종 이벤트 및 공연을 신청하실 수 있습니다.</dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/whitepaper/sameplepack.png" alt="">
+                            </figure>
+                            <dl>
+                                <dt>
+                                    <span>04</span>샘플팩 신청
+                                </dt>
+                                <dd>앙쥬에서 회원님들의 행복을 축하 드리며 임신부 용품은 물론 아기용품, 생활용품까지 다양한 선물을 드립니다.  신청 조건을 확인하시고 신청하세요.</dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <figure>
+                                <img src="/assets/images/whitepaper/angemall.png" alt="">
+                            </figure>
+                            <dl>
+                                <dt>
+                                    <span>05</span>앙쥬몰
+                                </dt>
+                                <dd>적립하는 재미! 사용하는 혜택! 마일리지는 앙쥬 콘텐츠의 다양한 활동을 통해 적립 받을 수 있으며 적립 받은 마일리지는 마일리지몰에서 현금처럼 사용할 수 있어 유아용품은 물론 생활용품, 장난감, 화장품 등을 구입하실 수 있습니다.</dd>
+                            </dl>
+                        </li>
+                    </ul>
+                    <div class="check">
+                        마일리지 쌓는법 확인하기
+                        <ul>
+                            <li>(바로가기 링크 / 고객센터</li>
+                            <li>자주하는 질문</li>
+                            <li>마일리지 쌓는법)</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+    
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot.php"); ?>
+    <script src="/assets/js/common.js"></script>
+</body>
+</html>
---
+++ whitepaper/long_time.php
@@ -0,0 +1,89 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead.php"); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/whitepaper.css" rel="stylesheet">
+</head>
+<body class="sub whitepaper"><!-- 폴더별 클래스명 추가 -->
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/header.php"); ?>
+    <!-- pgae-header -->
+    <div class="page-header">
+        <!-- 폴더별 타이틀 -->
+        <h2>오랜만의 방문이세요?</h2>
+        <div class="btn-controls">
+            <a href="#" class="prev">
+                <i class="icon-left-open-big">
+                    <span class="sr-only">이전 페이지</span>
+                </i>
+            </a>
+            <a href="#" class="next">
+                <i class="icon-right-open-big">
+                    <span class="sr-only">다음 페이지</span>
+                </i>
+            </a>
+        </div>
+    </div>
+    
+    <!-- sub content start -->
+    <main id="content">
+        <div class="container">
+            <section class="long-time"><!-- 페이지명 클래스 -->
+                <div class="section-content">
+                    <ul>
+                        <li>
+                            <div class="item">
+                                <img src="/assets/images/whitepaper/img_long_time01.png" alt="">
+                            </div>
+                            <div class="details">
+                                <h3>회원정보를 수정해주세요.</h3>
+                                <p>
+                                    각종 체험단 및 이벤트 신청을 위해회원님의 변경된 정보가 없는지 마이페이지를 통해 확인해주시고 회원 정보를 수정해주세요!
+                                </p>
+                            </div>
+                            <div class="btn-area">
+                                <p>
+                                    <a href="#" role="button" class="btn btn-pink">자세히 보기</a>
+                                </p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="item">
+                                <img src="/assets/images/whitepaper/img_long_time02.png" alt="">
+                            </div>
+                            <div class="details">
+                                <h3>기념일을 등록해주세요.</h3>
+                                <p>
+                                    결혼기념일, 엄마 생일,아이생일을추가 등록하시면 기념일에 맞게 마일리지가 자동 적립됩니다.
+                                </p>
+                            </div>
+                            <div class="btn-area">
+                                <p>
+                                    <a href="#" role="button" class="btn btn-pink">자세히 보기</a>
+                                </p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="item">
+                                <img src="/assets/images/whitepaper/img_long_time03.png" alt="">
+                            </div>
+                            <div class="details">
+                                <h3>관심 키워드를 선택해주세요.</h3>
+                                <p>
+                                    회원정보에서 관심 키워드를 선택하시면 그에 맞는 콘텐츠와  각종 체험단 및 이벤트 소식을  먼저 확인해  보실 수 있습니다.
+                                </p>
+                            </div>
+                            <div class="btn-area">
+                                <p>
+                                    <a href="#" role="button" class="btn btn-pink">자세히 보기</a>
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+    </main>
+    
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot.php"); ?>
+    <script src="/assets/js/common.js"></script>
+</body>
+</html>
