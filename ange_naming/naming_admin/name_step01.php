72ff2e727977fe79a1e1ddb77657052ee1fce164
kimyh5392 <mk.kimyh@gmail.com>
2018-02-19 14:50:49 +0900

    icon add

---
+++ ange_naming/naming_admin/name_step01.php
@@ -0,0 +1,103 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead.php"); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/ange_naming.css" rel="stylesheet">
+</head>
+<body class="sub ange-naming"><!-- 폴더별 클레스 -->
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/header.php"); ?>
+    
+    <!-- pgae-header -->
+    <div class="page-header">
+        <!-- 폴더별 타이틀 -->
+        <div class="page-title">
+            <h2>진행하기</h2>
+            <div class="btn-controls">
+                <a href="#" class="prev">
+                    <i class="icon-left-open-big">
+                        <span class="sr-only">이전 페이지</span>
+                    </i>
+                </a>
+                <a href="#" class="next">
+                    <i class="icon-right-open-big">
+                        <span class="sr-only">다음 페이지</span>
+                    </i>
+                </a>
+            </div>
+        </div>
+        <button type="button" class="drop-open-btn">
+            <span class="sr-only">메뉴 열기</span>
+        </button>
+    </div>
+    
+    <!-- sub content start -->
+    <main id="content">
+        <article class="name-step01">
+            <div class="container">
+                <div class="article-header">
+                    <div class="caution-box">
+                        <strong>작명 진행 안내</strong>
+                        <ol class="decimal-list">
+                            <li>로그인 → 신청서 작성 → 결제 → [진행하기] 메뉴 선택</li>
+                            <li>서비스는 30일 동안 이용 가능하며 서비스를 이용하신 후에는 <i>환불이 불가</i>합니다.
+                            </li>
+                        </ol>
+                        <p class="attention">
+                            추천 이름은 입력하신 데이터에 따라 다르며 (3~20개 사이) 추천 이름 데이터가 전혀 없는 경우는 관리자에게 연락주십시오 (네이밍 고객센터 : <a href="tel:02-333-4560">02-333-4560</a> 내선 1번)
+                        </p>
+                    </div>
+                    <div class="article-content">
+                        <h4 class="naming-title">작명/개명 당사자 오행정보</h4>
+                        <p class="summary">
+                            사주를 분석한 결과 나온 최상의 음오행입니다.
+                            성명학에서 중요한 대목의 하나로 경쾌하고 신나는 음악을 들을 때와,
+                            우울하고 슬픈 음악을 들을 때에 우리의 청각에 전달되고 뇌에 감지되는 
+                            음파장(音波長)의 작용이 다르듯 이름도 이와 같은 이치입니다.
+                            즉, 개인의 사조를 분석하고 성(姓)의 음오행(소리오행)을 기준하여
+                            성(姓)의 음오행과 상생(相生)으로 잘 연결된 음양오행을 구성합니다.
+                        </p>
+                        <div class="table-wrap default-table">
+                            <table class="table table-bordered">
+                                <colgroup>
+                                    <col style="width: 20%;">
+                                    <col style="width: 50%;">
+                                    <col style="width: 30%;">
+                                </colgroup>
+                                <thead>
+                                    <tr>
+                                        <th>번호</th>
+                                        <th>음오행(소리오행)</th>
+                                        <th>다음단계</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td class="syllable">목목화/木木火</td>
+                                        <td>
+                                            <div class="btn-proceed">
+                                                <a href="#" class="btn btn-xs btn-white" role="button">진행하기</a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td class="syllable">목목화/木木火</td>
+                                        <td>
+                                            <div class="btn-proceed">
+                                                <a href="#" class="btn btn-xs btn-white" role="button">진행하기</a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </main>
+    
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
+    <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot.php"); ?>
+    <script src="/assets/js/common.js"></script>
+</body>
+</html>
--- ange_naming/spacial_naming/name_author.php
+++ ange_naming/spacial_naming/name_author.php
@@ -30,11 +30,56 @@
     
     <!-- sub content start -->
     <main id="content">
-        <article class="spacial-intro">
+        <article class="name-author">
             <div class="container">
-                <ul>
+                <ul class="author-list">
                     <li>
-                        
+                        <figure>
+                            <img src="/assets/images/ange_naming/img_name_author01.jpg" class="img-responsive" alt="">
+                        </figure>
+                        <div class="details">
+                            <p>명리/관상/성명학/자미두수/기문을 두루 섭렵한 진정한 실력자</p>
+                            <h4><strong>공성윤</strong> 미래문제 연구소 대표원장</h4>
+                            <ul class="dot-list">
+                                <li>현)미래학회 회장</li>
+                                <li>현)미래문제연구소 분당본원 대표원장</li>
+                                <li>현)미래작명아카데미 분당본원 대표원장 </li>
+                                <li>현) 국제명리포럼 한국대표</li>
+                                <li>(INTERNATIONAL MINGLI FORUM) </li>
+                                <li>1977년 역학입문 </li>
+                                <li>역학 입문 이후 현재까지 400명 이상 문하생 배출 </li>
+                                <li>국제 역학 학술세미나 한국대표단으로 수차례 참가 </li>
+                                <li>전, 현직 대통령 관상해설(SBS 방송출연 동영상 보기) </li>
+                                <li>그 외 KBS, MBC,SBS, EBS역학관련 방송 다수 출연 </li>
+                                <li>정 재계, 고위공직인사들의 사주, 관상, 작명 분석가로 활동 </li>
+                                <li>&lt;저서&gt; 성명학 '복있는 이름은 이렇게 짓는다' 저자 </li>
+                                <li>사주학 핵심강의 (1,2,3권) 저자 </li>
+                                <li>사주실증사례집 120선 저자 </li>
+                                <li>'자미두수와 사주실증 감정사례' 공저 </li>
+                                <li>수상학 바로알기' 저자, '관상학 핵심요결' 저자 </li>
+
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <figure>
+                            <img src="/assets/images/ange_naming/img_name_author02.jpg" class="img-responsive" alt="">
+                        </figure>
+                        <div class="details">
+                            <p>평생 한학과 작명 연구에 몸담은 정통 작명가</p>
+                            <h4><strong>김정태</strong> 미래문제 연구소 소장</h4>
+                            <ul class="dot-list">
+                                <li>현)미래문제연구소 분당본원원장 </li>
+                                <li>현)미래작명아카데미 분당본원 원장 </li>
+                                <li>작명경력 40년, 서예경력 50년 </li>
+                                <li>주역,논어,맹자,시경,서경등 한학경력 40년 </li>
+                                <li>미래학회 작명담당 겸 성명학 교수</li> 
+                                <li>미래학회 학술위원 겸 고문 </li>
+                                <li>사단법인한국역리학회 중앙학술위원 </li>
+                                <li>&lt;저서&gt; 성명학 정통자원오행작명법 공동집</li>
+                                <li>필중(근간발행예정)</li>
+                            </ul>
+                        </div>
                     </li>
                 </ul>
             </div>
